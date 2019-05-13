3579f4954b718581e0c45a038a62b4656a3a2dd6
IkramMissaoui <missaoui.ikram726@gmail.com>
2019-05-13 13:36:10 +0200

    Updates

---
+++ precache-manifest.af730b43a34c5090917a0cb257ef44d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7e9438fbff5878aba3459fc9200c6bc",
+    "url": "/credit-card/index.html"
+  },
+  {
+    "revision": "8800da6e68373cc5d3c6",
+    "url": "/credit-card/static/css/main.9e27bb97.chunk.css"
+  },
+  {
+    "revision": "43d1f55c2a696b4563b8",
+    "url": "/credit-card/static/js/2.179f4c70.chunk.js"
+  },
+  {
+    "revision": "8800da6e68373cc5d3c6",
+    "url": "/credit-card/static/js/main.f24cf786.chunk.js"
+  },
+  {
+    "revision": "2a4b22a3002466356f44",
+    "url": "/credit-card/static/js/runtime~main.0914b2aa.js"
+  },
+  {
+    "revision": "2bd8653315acb7c8ae4c485f34984c29",
+    "url": "/credit-card/static/media/back.2bd86533.jpg"
+  },
+  {
+    "revision": "4bfd75adb187034c3460d2907ac2bfe1",
+    "url": "/credit-card/static/media/imb.4bfd75ad.png"
+  },
+  {
+    "revision": "963cd3ae25125be849f30c8e1cc3177f",
+    "url": "/credit-card/static/media/lastimage.963cd3ae.png"
+  }
+]);
